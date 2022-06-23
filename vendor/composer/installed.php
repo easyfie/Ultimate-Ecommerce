87410a21fa315e37533884b64f30b7e368e4319d
jahid218875 <jahid218875@gmail.com>
2022-06-23 16:47:31 +0600

    theme upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'robi/easyfieapi' => array(
+            'pretty_version' => 'v2.5',
+            'version' => '2.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robi/easyfieapi',
+            'aliases' => array(),
+            'reference' => '800edd53d039e876a131ffe64dd5b4e56383dbd2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
